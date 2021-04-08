f764a4e222d98efce1702161fc9cf36943d0e663
NIKI NIKOLOV <nikinikolov99@yahoo.com>
2021-04-08 22:40:41 +0300

    inital commit

---
+++ mpg/mpg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
